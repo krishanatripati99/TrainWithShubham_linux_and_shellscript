fc20fbf20e6c84931071adfa68c7738a440f2393
Ubuntu <ubuntu@ip-172-31-40-17.ec2.internal>
2023-11-07 16:05:48 +0000

    3rd video

---
+++ shellscript_tutorial/devops_first.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+echo "I will be a great devops engineer"
+
+echo $Bash
+
+name="krishna"
+
+echo "hello $name, please enter your age"
+
+read age
+
+echo "My name is ${age}"
+
+echo "sub: hello my name is $1"
+
+sleep 2
+
+echo "welcome to our channel"
+
+sleep 2
+
+echo "please subscribe"
---
+++ shellscript_tutorial/if_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$1" = "like" ]
+then
+ echo "hey please $1 the video"
+else
+ echo "please subscribe"
+fi
